c691b4e2b5e0ad11dbfbee65fc6854307ea8e025
disaster37 <linuxworkgroup@hotmail.com>
2024-11-08 18:27:26 +0000

    fix: fix ci
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- cluster-test/cluster-test.go
+++ cluster-test/cluster-test.go
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a MIT-license.
 // See http://olivere.mit-license.org/license.txt for details.
 
-// nolint:all
-package main
+package test
 
 import (
 	"context"
