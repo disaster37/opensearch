37f5ae884a46a2d240d959e66728451ed68f0dd3
disaster37 <linuxworkgroup@hotmail.com>
2026-05-26 15:13:29 +0000

    fix: fix tests

--- indices_data_stream_test.go
+++ indices_data_stream_test.go
@@ -15,9 +15,9 @@ func TestIndicesDatastream(t *testing.T) {
 	// Create template for datastream index
 	createTemplate, err := client.IndexPutIndexTemplate(testDatastreamIndexName).
 		BodyString(`{
-			"index_patterns": ["` + testDatastreamIndexName + `*"],
+			"index_patterns": ["` + testDatastreamIndexName + `"],
 			"data_stream": {},
-			"priority": 1,
+			"priority": 1
 		}`).Do(context.TODO())
 	if err != nil {
 		t.Fatal(err)
